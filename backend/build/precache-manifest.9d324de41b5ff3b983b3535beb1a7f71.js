03418ef00b783ebc6439179813ef3e5af7c1cf2c
Thomas Girgenti <thomasg@disroot.org>
2019-12-19 23:27:20 -0700

    configure backend for app engine

---
+++ backend/build/precache-manifest.9d324de41b5ff3b983b3535beb1a7f71.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20a700e87d143a8c283a2259d3bfb299",
+    "url": "/index.html"
+  },
+  {
+    "revision": "14075607d0c793f35cf7",
+    "url": "/static/css/main.99c920fa.chunk.css"
+  },
+  {
+    "revision": "dd33c526e48f270cf0b0",
+    "url": "/static/js/2.4cddd379.chunk.js"
+  },
+  {
+    "revision": "4c4a2d94529dbd7ce5ce1410efb3ac39",
+    "url": "/static/js/2.4cddd379.chunk.js.LICENSE"
+  },
+  {
+    "revision": "14075607d0c793f35cf7",
+    "url": "/static/js/main.b2534ceb.chunk.js"
+  },
+  {
+    "revision": "a232a10c865a9eb4a435",
+    "url": "/static/js/runtime-main.72af4ec0.js"
+  },
+  {
+    "revision": "40a53849a29e060686619a19237066d5",
+    "url": "/static/media/Manrope-Bold.40a53849.woff2"
+  },
+  {
+    "revision": "8f960c59075a2ee98f96df94aaf1f2a5",
+    "url": "/static/media/Manrope-ExtraBold.8f960c59.woff2"
+  },
+  {
+    "revision": "2557cb624f9cc8639a82b3212a1c8100",
+    "url": "/static/media/Manrope-ExtraLight.2557cb62.woff2"
+  },
+  {
+    "revision": "9aa133c630cd26e7bebc2ea9803d4113",
+    "url": "/static/media/Manrope-Light.9aa133c6.woff2"
+  },
+  {
+    "revision": "d484e4d389b71f875ca186f6ecd19816",
+    "url": "/static/media/Manrope-Medium.d484e4d3.woff2"
+  },
+  {
+    "revision": "1b6a58036590640a5d7050ddab7e38ec",
+    "url": "/static/media/Manrope-Regular.1b6a5803.woff2"
+  },
+  {
+    "revision": "46cd7793d53e53f36707119b96f10957",
+    "url": "/static/media/Manrope-SemiBold.46cd7793.woff2"
+  }
+]);
--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,35 @@
+require('@google-cloud/trace-agent').start();
+require('@google-cloud/profiler').start();
+require('@google-cloud/debug-agent').start();
+
 require('dotenv').config();
 const cors = require('cors');
-const morgan = require('morgan');
+const path = require('path');
+const helmet = require('helmet');
+const winston = require('winston');
+const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const user = require('./routes/user');
+const rateLimit = require("express-rate-limit");
+const {LoggingWinston} = require('@google-cloud/logging-winston');
 const doc = require('./routes/doc');
+const user = require('./routes/user');
 
-
-const express = require('express');
 
 const app = express();
-
 const PORT = process.env.PORT;
 
 
-app.use(cors());
-app.use(morgan('dev'));
-app.use(bodyParser.json());
+const loggingWinston = new LoggingWinston();
+const logger = winston.createLogger({
+	level: 'info',
+	transports: [
+		new winston.transports.Console(),
+		loggingWinston
+	],
+});
+logger.error('Winston error!');
+logger.info('Winston info');
 
 
 mongoose.connect(process.env.MONGO_URI, {
@@ -24,10 +37,44 @@ mongoose.connect(process.env.MONGO_URI, {
 	useFindAndModify: false,
 	useUnifiedTopology: true
 }).catch(error => console.log(`Initial connection error: ${{error}}`));
-
 const db = mongoose.connection;
 db.once('open', () => console.log('Successfully connected to database'));
 db.on('error', err => console.error.bind(console, `Database runtime error: ${err}`));
+
+
+app.set('trust proxy', true);
+app.use((req, res, next) => {
+	if (req.secure) {
+		next();
+	} else {
+		res.redirect('https://' + req.headers.host + req.url);
+	}
+});
+
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+
+
+const userLimiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 60m
+	max: 10
+});
+
+const docLimiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 60m
+	max: 100
+});
+
+const genLimiter = rateLimit({
+	windowMs: 60 * 60 * 1000, // 60m
+	max: 200
+});
+
+app.use('/api/user', userLimiter);
+app.use('/api/doc', docLimiter);
+app.use('/*', genLimiter);
 
 
 app.use('/api/user', user);
@@ -35,7 +82,7 @@ app.use('/api/doc', doc);
 
 
 app.get('/*', (req, res) => {
-	res.json({message: 'Invalid endpoint'});
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 
